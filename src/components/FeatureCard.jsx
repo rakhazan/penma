7bcde1d5f7078b0c47a2a65126e3471af07bccfc
rakhazan <rakhazahnur26@gmail.com>
2023-01-05 15:59:02 +0700

    :lipstick: customise font size, padding, and margin in medium screen

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const FeatureCard = ({ icon, heading, desc, index }) => (
   <>
-    <div className="flex flex-col items-start gap-2 max-w-sm bg-white rounded-3xl px-4 py-6">
+    <div className="flex flex-col items-start gap-2 flex-none w-full max-w-sm bg-white rounded-3xl px-4 py-6">
       <img src={icon} alt={`icon ${index}`} className="h-24" />
-      <h5 className="text-sm text-black font-semibold">{heading}</h5>
-      <p className="text-xs">{desc}</p>
+      <h5 className="text-sm md:text-lg text-black font-semibold">{heading}</h5>
+      <p className="text-xs md:text-base">{desc}</p>
     </div>
   </>
 );
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -3,8 +3,10 @@ import { ImQuotesRight } from "react-icons/im";
 
 const TestimonialCard = ({ username, text }) => (
   <>
-    <div className="flex flex-col gap-4 max-w-sm px-4 py-6 bg-white rounded-2xl drop-shadow-xl">
-      <p className="text-sm text-black leading-relaxed">{text}</p>
+    <div className="flex flex-col gap-4 max-w-sm px-4 sm:px-6 py-6 bg-white rounded-2xl drop-shadow-xl">
+      <p className="text-sm sm:text-base md:text-lg text-black leading-relaxed">
+        {text}
+      </p>
       <span className="text-end">
         <ImQuotesRight />
       </span>
@@ -12,9 +14,11 @@ const TestimonialCard = ({ username, text }) => (
         <img
           src="/src/assets/default-user.png"
           alt="user"
-          className="w-10 h-10"
+          className="w-10 md:w-12 h-10 md:h-12"
         />
-        <p className="text-sm text-black font-medium">{username}</p>
+        <p className="text-sm sm:text-lg md:text-xl text-black font-medium">
+          {username}
+        </p>
       </div>
     </div>
   </>
